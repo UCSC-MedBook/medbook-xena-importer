fe2a2df08c5968ab4eb07ad32737224c348cdb17
Mike Risse <rissem@gmail.com>
2016-12-08 01:40:02 +0000

    update to latest JAR (0.21.0)

--- import.sh
+++ import.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Usage: `./import.sh PATH` (relative to /home/ubutu/xena_data, e.g. `./import.sh HiSeqV2_PANCAN`
-docker exec xena java -jar /ucsc_xena/cavm-0.20.0-standalone.jar --load /root/xena/files/${1}
+docker exec xena java -jar /ucsc_xena/cavm-0.21.0-standalone.jar --load /root/xena/files/${1}
